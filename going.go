4c35e0ba43a1fc92577f3fbab10e1466b60c0de5
Glenn Yonemitsu <yonemitsu@gmail.com>
2015-08-08 15:50:15 -0400

    config.go code moved to going.go since it is all going server related stuff

--- going.go
+++ going.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 	"path/filepath"
@@ -13,6 +14,15 @@ type going struct {
 	logger *log.Logger
 	// key = yaml base filename without extension
 	programs map[string]*Program
+}
+
+type goingConfig struct {
+	Log              logConfig
+	PidFile          string
+	ProgramConfigDir string
+	SocketPath       string
+	Umask            int
+	Username         string
 }
 
 // wish it was named getGoing, but consistency trumps all
@@ -110,4 +120,55 @@ func (g *going) listen() {
 	for {
 		time.Sleep(500 * time.Millisecond)
 	}
+}
+
+// Search order for config file in this order:
+// global variable flagConfigFile, aka command line -config flag
+// environment variable named in const EnvVarConfigFile
+// $HOME/.going.conf,
+// /etc/going.conf
+func findGoingConfigFile() (string, error) {
+	if isValidFile(*flagConfigFile) {
+		return *flagConfigFile, nil
+	}
+
+	envVar := os.Getenv(EnvVarConfigFile)
+	if isValidFile(envVar) {
+		return envVar, nil
+	}
+
+	home := os.Getenv("HOME")
+	homeConfig := filepath.Join(home, ".going", "going.conf")
+	if isValidFile(homeConfig) {
+		return homeConfig, nil
+	}
+
+	etcConfig := "/etc/going/going.conf"
+	if isValidFile(etcConfig) {
+		return etcConfig, nil
+	}
+
+	return "", errors.New("Could not find config file.")
+}
+
+func newGoingConfig(filename string) (*goingConfig, error) {
+	c := new(goingConfig)
+	err := loadYaml(filename, c)
+
+	configDir := filepath.Dir(filename)
+
+	// set defaults
+	if c.PidFile == "" {
+		c.PidFile = filepath.Join(configDir, "going.pid")
+	}
+
+	if c.ProgramConfigDir == "" {
+		c.ProgramConfigDir = filepath.Join(configDir, "programs")
+	}
+
+	if c.Log.Dir == "" {
+		c.Log.Dir = filepath.Join(configDir, "logs")
+	}
+
+	return c, err
 }
